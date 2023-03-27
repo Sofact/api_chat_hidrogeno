bef9bc14a42dc1968116d5b61085c8afa7d5b2ee
Puntonatural <116592883+Puntonatural@users.noreply.github.com>
2023-03-26 23:46:59 -0500

    usr_avatar

--- app/Http/Resources/User/ProfileUserGeneralResource.php
+++ app/Http/Resources/User/ProfileUserGeneralResource.php
@@ -18,7 +18,7 @@ class ProfileUserGeneralResource extends JsonResource
             "name"=>$this->resource->name,
             "surname" =>$this->resource->surname,
             "email" =>$this->resource->email,
-            "avatar"=> $this->resource->acatar ? env("APP_URL")."storage/".$this->resource->avatar : "https://www.flaticon.es/icono-gratis/usuario_1177568?term=usuario&page=1&position=2&origin=tag&related_id=1177568"
+            "usr_avatar"=> $this->resource->acatar ? env("APP_URL")."storage/".$this->resource->avatar : "https://www.flaticon.es/icono-gratis/usuario_1177568?term=usuario&page=1&position=2&origin=tag&related_id=1177568"
 
         ];
     }
